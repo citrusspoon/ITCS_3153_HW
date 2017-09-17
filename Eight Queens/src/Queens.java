9c7a72d47bf85a41d751cb6cb2fc9413cad03efc
Scott Schreiber <citrusspoon@gmail.com>
2017-09-17 14:24:16 -0400

    Setup project

---
+++ Eight Queens/src/Queens.java
@@ -0,0 +1,22 @@
+
+public class Queens {
+
+	public static void main(String[] args) {
+		
+		int[][] board = new int[8][8];
+		
+		
+		
+		
+
+	}
+	
+	public static void printBoard(int[][] b) {
+		for(int i = 0; i < b.length; i++) {
+			for(int j = 0; j < b[i].length; j++)
+				System.out.print(b[i][j] + " ");
+			System.out.print("\n");
+		}
+	}
+
+}
