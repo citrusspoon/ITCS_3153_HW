c149e1f6ccfba8ac0ae5a4e86c6004968e3d1f5f
Scott Schreiber <citrusspoon@gmail.com>
2017-11-14 21:27:19 -0500

    Started

--- Bomber Belts/Bomber Belts 2015/Bomber Belts 2015/Assets/Resources/AI Scripts/AI_ScottSchreiber.cs
+++ Bomber Belts/Bomber Belts 2015/Bomber Belts 2015/Assets/Resources/AI Scripts/AI_ScottSchreiber.cs
@@ -15,10 +15,17 @@ public class AI_ScottSchreiber : MonoBehaviour {
 	//Global variables 
 	private float bottomBound = -8f;
 	private float topBound = 8f;
-	private float bombPlayerBound = 0f;
+	private float bombCharacterBound = 0f;
 	private float bombOpponentBound = 18f;
 	private int[] intButtonPositions;
+	private int intCharacterPosition;
+	private int convertedCharacterPosition;
 	private int targetButtonIndex;
+	private bool enRoute;
+	private int previousPosition;
+
+	private float fullBaseBombTraversalTime = 8f;
+	private float slowestCharacterFullTraversalTime = 6f;
 
 	// Use this for initialization
 	void Start () {
@@ -39,9 +46,16 @@ public class AI_ScottSchreiber : MonoBehaviour {
 
 
 		intButtonPositions = new int[8];
+		enRoute = true;
 
 		for (int i = 0; i < mainScript.getButtonLocations ().Length; i++)
 			intButtonPositions [i] = (int)mainScript.getButtonLocations () [i];
+
+		convertedCharacterPosition = ConvertCharacterPosition ();
+		if (convertedCharacterPosition > 0)
+			targetButtonIndex = 0;
+		else
+			targetButtonIndex = 7;
 	}
 
 	// Update is called once per frame
@@ -59,6 +73,8 @@ public class AI_ScottSchreiber : MonoBehaviour {
 			Button positions are between -8.4 and 8.4, with 2.4 between them
 			Character position is between -8.58 and 8.58
 			For bomb distance, character side is 0, middle is ~9 and opponent side is ~18
+			Takes character 6 seconds to fully cross all buttons while pushing every one, 4 seconds without 
+			Takes bomb 4 seconds to get from middle of belt to other side at base speed
 		*/
 		RunAI();
 
@@ -66,9 +82,80 @@ public class AI_ScottSchreiber : MonoBehaviour {
 	}
 
 	void RunAI(){
+		intCharacterPosition = (int)mainScript.getCharacterLocation ();
+		convertedCharacterPosition = ConvertCharacterPosition ();
+
+
+		//iterate through buttons to find the closest bomb to me that is reachable
+		//move to that belt while pressing buttons on the way
+
+
+		//find next target button
+		if (!enRoute) {
+			for (int i = 0; i < mainScript.getButtonLocations ().Length; i++) {
+
+
+
+
+
+			}
+		}
+
+
+
+
+		//move to target button
+
+		if (targetButtonIndex > convertedCharacterPosition)
+			mainScript.moveUp ();
+		else if (targetButtonIndex < convertedCharacterPosition)
+			mainScript.moveDown ();
+		else
+			enRoute = false;
 		
+		
+			
+		//push buttons on the way and at destination
+		if (convertedCharacterPosition >-1 && mainScript.getBeltDirections()[convertedCharacterPosition] <=0 && mainScript.getButtonCooldowns()[convertedCharacterPosition] <=0)
+			mainScript.push ();
+		print ("pos: " + mainScript.getBombDistances()[0]);
+		print ("b speed: " + mainScript.getBombSpeeds()[0]);
 
+	
 
+	}
+
+	float GetBombTimeToExplode(int beltIndex){
+		
+		return 0f;
+	}
+
+	/// <summary>
+	/// Converts the character's z position into an index from 0-7 that lines up with the buttons
+	/// </summary>
+	/// <returns>The character position. an index from 0-7</returns>
+	int ConvertCharacterPosition(){
+
+		switch (intCharacterPosition) {
+		case -8:
+			return 0;
+		case -6:
+			return 1;
+		case -3:
+			return 2;
+		case -1:
+			return 3;
+		case 1:
+			return 4;
+		case 3:
+			return 5;
+		case 6:
+			return 6;
+		case 8:
+			return 7;
+		default:
+			return convertedCharacterPosition;
+		}
 	}
 
 
